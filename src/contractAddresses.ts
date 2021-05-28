fffcea680a5406fdfba22dfe4715bc2a84c72e15
버니 <bunny@beoniui-MacBookPro.local>
2021-05-28 11:54:48 +0900

    added more pools and deleted abis

---
+++ src/contractAddresses.ts
@@ -0,0 +1,14 @@
+export const poolsAndTokens = {
+    mainnet: {
+        Bunny: '0xC9849E6fdB743d08fAeE3E34dd2D1bc69EA11a51',
+        BunnyPool:'0xCADc8CB26c8C7cB46500E61171b5F27e9bd7889D',
+        BunnyBoostPool: '0xb037581cF0cE10b04C4735443d95e0C93db5d940',
+        CakeBnbFlip: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
+        CakeBnbFlipCakeMaximizerPool: '0x203Ee29ba85BbDfA23bFaE5D77620AeFDaf92cB1',
+        UsdtBnbFlip: '0x16b9a82891338f9bA80E2D6970FddA79D1eb0daE',
+        UsdtBnbFlipCakeMaximizerPool: '0xac7D40638271D349cb2DeDfdC6268A77738403Fa',
+        BusdBnbFlip: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+        BusdBnbFlipCakeMaximizerPool: '0x81fa6F9F4599c5316Cc53B782DE7c01EFf4f9551',
+        DashboardBsc: '0xb3C96d3C3d643c2318E4CDD0a9A48aF53131F5f4'
+    },
+};
--- src/poolsAndTokens.ts
+++
@@ -1,6 +0,0 @@
-export const tokens = {
-    mainnet: {
-        Bunny: '0xC9849E6fdB743d08fAeE3E34dd2D1bc69EA11a51',
-        BunnyPool:'0xCADc8CB26c8C7cB46500E61171b5F27e9bd7889D',
-    },
-};
